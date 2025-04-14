5f1a8eb63713791695d95f2b1c74166834bcf1b5
Armandas Barkauskas <armandas.bark@gmail.com>
2025-04-14 19:56:57 +0300

    Configured .NET Aspire Dashboard

--- AppHost/Program.cs
+++ AppHost/Program.cs
@@ -1,3 +1,9 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
+builder.AddProject<Projects.CurrencyRate_API>("currency-rate");
+builder.AddProject<Projects.FinanceMaster_API>("finance-master");
+builder.AddProject<Projects.Identity_API>("identity");
+builder.AddProject<Projects.Import_API>("import");
+builder.AddProject<Projects.Prediction_API>("prediction");
+
 builder.Build().Run();
